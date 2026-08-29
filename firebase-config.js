41311ff466dc27fc4c2d762adc73260e41f14444
6woL337 <w82733037@gmail.com>
2026-08-29 11:09:28 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,19 +6,6 @@ import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
 
-import {
-  getStorage
-} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-storage.js";
-
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAoihL9o8PbsQIZDeU_13G0hUBd0qIqq8I",
   authDomain: "comic-viewer-4a317.firebaseapp.com",
@@ -30,6 +17,15 @@ const firebaseConfig = {
   measurementId: "G-MQXQFTWFN9"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app =
+  initializeApp(firebaseConfig);
+
+
+const db =
+  getFirestore(app);
+
+
+export {
+  app,
+  db
+};
